a0371906fb8feba0fd1c13a33f5f7bb0541fd6cb
Michael T. Gillespie <mtgillespie@facettech.com>
2016-03-15 08:32:38 -0500

    Initial commit for GitHub.

---
+++ hidden-tear-bruteforcer/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace hidden_tear_bruteforcer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bruteforceButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openSampleFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.FileButton = new System.Windows.Forms.Button();
+            this.FileSelectedLabel = new System.Windows.Forms.Label();
+            this.DisplayText = new System.Windows.Forms.TextBox();
+            this.AttemptsLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.InfoLabel = new System.Windows.Forms.Label();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadKeyListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openKeyListFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bruteforceButton
+            // 
+            this.bruteforceButton.Location = new System.Drawing.Point(72, 131);
+            this.bruteforceButton.Name = "bruteforceButton";
+            this.bruteforceButton.Size = new System.Drawing.Size(211, 31);
+            this.bruteforceButton.TabIndex = 2;
+            this.bruteforceButton.Text = "Start Bruteforce";
+            this.bruteforceButton.UseVisualStyleBackColor = true;
+            this.bruteforceButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 281);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hidden Tear Bruteforcer\r\ncoded by Michael Gillespie / Demonslay335";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label3.Location = new System.Drawing.Point(131, 218);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Key Found!";
+            this.label3.Visible = false;
+            // 
+            // openSampleFileDialog
+            // 
+            this.openSampleFileDialog.Filter = "Encrypted PNG (*.png.*)|*.png.*";
+            this.openSampleFileDialog.Title = "Select Encrypted Sample";
+            // 
+            // FileButton
+            // 
+            this.FileButton.Location = new System.Drawing.Point(106, 71);
+            this.FileButton.Name = "FileButton";
+            this.FileButton.Size = new System.Drawing.Size(143, 31);
+            this.FileButton.TabIndex = 1;
+            this.FileButton.Text = "Browse Sample";
+            this.FileButton.UseVisualStyleBackColor = true;
+            this.FileButton.Click += new System.EventHandler(this.FileButton_Click);
+            // 
+            // FileSelectedLabel
+            // 
+            this.FileSelectedLabel.AutoSize = true;
+            this.FileSelectedLabel.Location = new System.Drawing.Point(135, 105);
+            this.FileSelectedLabel.Name = "FileSelectedLabel";
+            this.FileSelectedLabel.Size = new System.Drawing.Size(85, 13);
+            this.FileSelectedLabel.TabIndex = 2;
+            this.FileSelectedLabel.Text = "No File Selected";
+            // 
+            // DisplayText
+            // 
+            this.DisplayText.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisplayText.Location = new System.Drawing.Point(13, 195);
+            this.DisplayText.Name = "DisplayText";
+            this.DisplayText.ReadOnly = true;
+            this.DisplayText.Size = new System.Drawing.Size(329, 20);
+            this.DisplayText.TabIndex = 6;
+            // 
+            // AttemptsLabel
+            // 
+            this.AttemptsLabel.AutoSize = true;
+            this.AttemptsLabel.Location = new System.Drawing.Point(150, 173);
+            this.AttemptsLabel.Name = "AttemptsLabel";
+            this.AttemptsLabel.Size = new System.Drawing.Size(54, 13);
+            this.AttemptsLabel.TabIndex = 7;
+            this.AttemptsLabel.Text = "Attempts: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 39);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Please select a sample encrypted PNG file.\r\nThe smaller the file, the faster the " +
+    "bruteforce will run.\r\nPreferably a file less than 1KB.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // InfoLabel
+            // 
+            this.InfoLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.InfoLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InfoLabel.Location = new System.Drawing.Point(13, 238);
+            this.InfoLabel.Name = "InfoLabel";
+            this.InfoLabel.Size = new System.Drawing.Size(329, 38);
+            this.InfoLabel.TabIndex = 9;
+            this.InfoLabel.Text = "Info";
+            this.InfoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.InfoLabel.Visible = false;
+            this.InfoLabel.Click += new System.EventHandler(this.InfoLabel_Click);
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.mainMenu.Size = new System.Drawing.Size(354, 24);
+            this.mainMenu.TabIndex = 10;
+            this.mainMenu.Text = "mainMenu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadKeyListToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadKeyListToolStripMenuItem
+            // 
+            this.loadKeyListToolStripMenuItem.Name = "loadKeyListToolStripMenuItem";
+            this.loadKeyListToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadKeyListToolStripMenuItem.Text = "Load Key List";
+            this.loadKeyListToolStripMenuItem.Click += new System.EventHandler(this.loadKeyListToolStripMenuItem_Click);
+            // 
+            // openKeyListFileDialog
+            // 
+            this.openKeyListFileDialog.Filter = "Key List (*.csv)|*.csv";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 325);
+            this.Controls.Add(this.InfoLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AttemptsLabel);
+            this.Controls.Add(this.DisplayText);
+            this.Controls.Add(this.FileSelectedLabel);
+            this.Controls.Add(this.FileButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bruteforceButton);
+            this.Controls.Add(this.mainMenu);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mainMenu;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "HT BruteForcer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bruteforceButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openSampleFileDialog;
+        private System.Windows.Forms.Button FileButton;
+        private System.Windows.Forms.Label FileSelectedLabel;
+        private System.Windows.Forms.TextBox DisplayText;
+        private System.Windows.Forms.Label AttemptsLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label InfoLabel;
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadKeyListToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openKeyListFileDialog;
+    }
+}
+
---
+++ hidden-tear-bruteforcer/Form1.cs
@@ -0,0 +1,372 @@
+﻿/*
+ _     _     _     _              _                  
+| |   (_)   | |   | |            | |                 
+| |__  _  __| | __| | ___ _ __   | |_ ___  __ _ _ __ 
+| '_ \| |/ _` |/ _` |/ _ \ '_ \  | __/ _ \/ _` | '__|
+| | | | | (_| | (_| |  __/ | | | | ||  __/ (_| | |   
+|_| |_|_|\__,_|\__,_|\___|_| |_|  \__\___|\__,_|_| 
+ * Coded by Utku Sen(Jani) / August 2015 Istanbul / utkusen.com 
+ * hidden tear may be used only for Educational Purposes. Do not use it as a ransomware!
+ * You could go to jail on obstruction of justice charges just for running hidden tear, even though you are innocent.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security;
+using System.Security.Cryptography;
+using System.IO;
+using System.Net;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace hidden_tear_bruteforcer
+{
+
+    public partial class Form1 : Form
+    {
+        
+        String sampleFileName;
+        static String keyListFileName;
+        static String decryptedFileName;
+
+        // Attempts run
+        static int attempts = 0;
+
+        // Keys loaded from file
+        static Queue<String> keyList = new Queue<String>();
+
+        // PNG magic byte header
+        static byte[] PNG_MAGIC_BYTES = new byte[8] { 137, 80, 78, 71, 13, 10, 26, 10 };
+
+        // Background worker
+        static BackgroundWorker worker = new BackgroundWorker();
+
+        // Hash instance
+        static SHA256 sha256 = SHA256.Create();
+
+        // Set your salt here, change it to meet your flavor:
+        // The salt bytes must be at least 8 bytes.
+        static byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        static string seed = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890*/&%!=";
+
+        // AES instance
+        static RijndaelManaged AES = new RijndaelManaged
+        {
+            KeySize = 256,
+            BlockSize = 128,
+            Mode = CipherMode.CBC
+        };
+
+        static int keyBytes = AES.KeySize / 8;
+        static int ivBytes = AES.BlockSize / 8;
+
+        // RNG instance
+        static RNGCryptoServiceProvider rNGCryptoServiceProvider = new RNGCryptoServiceProvider();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static int GetInt(RNGCryptoServiceProvider rnd, int max)
+        {
+            byte[] array = new byte[4];
+            int num;
+            do
+            {
+                rnd.GetBytes(array);
+                num = (BitConverter.ToInt32(array, 0) & 2147483647);
+            }
+            while (num >= max * (2147483647 / max));
+            return num % max;
+        }
+
+        public static string CreatePassword(int length)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            
+            while (length-- > 0)
+            {
+                stringBuilder.Append(seed[Form1.GetInt(rNGCryptoServiceProvider, seed.Length)]);
+            }
+            return stringBuilder.ToString();
+        }
+
+        static public byte[] AES_Decrypt(byte[] bytesToBeDecrypted, byte[] passwordBytes)
+        {
+            byte[] decryptedBytes = null;
+
+            try {
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(keyBytes);
+                    AES.IV = key.GetBytes(ivBytes);
+                    
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                        cs.Close();
+                    }
+                    decryptedBytes = ms.ToArray();
+                    
+                }
+            }
+            catch(Exception e)
+            {
+                
+            }
+
+            return decryptedBytes;
+        }
+
+        private void StartBruteForce()
+        {
+            // Display attempts label
+            DisplayText.Text = "Setting up...";
+            DisplayText.Visible = true;
+
+            Log("Starting brute force on " + sampleFileName);
+
+            // Setup background worker thread
+            worker.WorkerReportsProgress = true;
+            worker.WorkerSupportsCancellation = true;
+
+            worker.DoWork += BruteForce;
+            worker.RunWorkerCompleted += RunWorkerCompleted;
+            worker.ProgressChanged += ProgressChanged;
+            worker.RunWorkerAsync(sampleFileName);
+
+        }
+
+        static private void BruteForce(object sender, DoWorkEventArgs e)
+        {
+
+            String testFile = e.Argument.ToString();
+
+            // Load the sample file into memory
+            byte[] bytesToBeDecrypted = File.ReadAllBytes(testFile);
+
+            // Password buffer
+            string passwordAttempt = "";
+
+            // Password found flag
+            bool passwordFound = false;
+
+            // Byte buffer
+            byte[] bytesFileMagic = new byte[8];
+
+            while (true)
+            {
+                attempts++;
+                // Check for cancel
+                if (worker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+
+                // Check for a loaded key list
+                if (keyListFileName == null)
+                {
+                    // Generate a random password
+                    passwordAttempt = CreatePassword(32);
+                    //passwordAttempt = "VrtiGxUbI8afaJwGbkePtpJKINyGIkZC";
+                }
+                else
+                {
+                    // Get next key from key list
+                    passwordAttempt = GetNextKey();
+                }
+
+                // Check to break on end of key file
+                if (passwordAttempt == null)
+                {
+                    throw new Exception("Reached end of keylist");
+                }
+
+                // Report progress
+                try {
+                    worker.ReportProgress(0, passwordAttempt);
+                }
+                catch(Exception)
+                {
+                    break;
+                }
+                
+                // Hash password bytes
+                byte[] passwordBytes = Encoding.UTF8.GetBytes(passwordAttempt);
+                passwordBytes = sha256.ComputeHash(passwordBytes);
+                
+                // Decrypt with password
+                byte[] bytesDecrypted = AES_Decrypt(bytesToBeDecrypted, passwordBytes);
+                
+                // Check for failed decryption
+                if (bytesDecrypted == null)
+                {
+                   continue;
+                }
+                
+                // Get first 8 bytes of "decrypted" file
+                Array.Copy(bytesDecrypted, bytesFileMagic, 8);
+                
+                // Check for PNG
+                if(bytesFileMagic.SequenceEqual(PNG_MAGIC_BYTES))
+                {
+                    passwordFound = true;
+                    decryptedFileName = Path.GetDirectoryName(testFile) + "\\decrypted-" + Path.GetFileNameWithoutExtension(testFile);
+                    FileStream fileStream = new FileStream(decryptedFileName, FileMode.Create, FileAccess.Write);
+                    fileStream.Write(bytesDecrypted, 0, bytesDecrypted.Length);
+                    fileStream.Close();
+                    break;
+                }
+                else
+                {
+//                    Log("Possible password for " + testFile + ": " + passwordAttempt);
+                }
+                
+            }
+
+            if (passwordFound)
+            {
+                e.Result = passwordAttempt;
+            }
+
+        }
+
+        void ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            DisplayText.Text = "Attempting: " + e.UserState.ToString();
+            AttemptsLabel.Text = "Attempts: " + attempts;
+        }
+
+        void RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                DisplayText.Text = "Error: " + e.Error.Message;
+                Log("Error: " + e.Error.ToString());
+            }
+            else if(!e.Cancelled)
+            {
+                label3.Visible = true;
+                DisplayText.Text = "Password: " + e.Result.ToString();
+                InfoLabel.Visible = true;
+                InfoLabel.Text = "Click here to check file for success:\r\n" + Path.GetFileName(decryptedFileName);
+
+                bruteforceButton.Enabled = false;
+                Log("Password found for " + sampleFileName + " (" + attempts + " attempts): " + e.Result.ToString());
+            }
+            else
+            {
+                Log("Bruteforce stopped");
+            }
+        }
+
+        static String GetNextKey()
+        {
+            if (keyList.Count == 0)
+                return null;
+
+            return keyList.Dequeue();
+
+        }
+
+        static void Log(String text)
+        {
+            using (StreamWriter log = new StreamWriter("log.txt", true))
+            {
+                log.WriteLine(text);
+            }
+        }
+        private void InfoLabel_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(decryptedFileName);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(this.sampleFileName == null)
+            {
+                DisplayText.Text = "Please select an encrypted file sample";
+                return;
+            }
+
+            if (!worker.IsBusy)
+            {
+                bruteforceButton.Text = "Pause Bruteforce";
+                FileButton.Enabled = false;
+                StartBruteForce();                
+            }
+            else
+            {
+                bruteforceButton.Text = "Start Bruteforce";
+                FileButton.Enabled = true;
+                worker.CancelAsync();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+            
+            this.Text = String.Format("HT BruteForcer v{0}", version);
+        }
+
+        private int LoadKeyList(String keyListFile)
+        {
+            // Clear key list
+            keyList.Clear();
+
+            int rows = 0;
+            using(StreamReader sr = new StreamReader(keyListFile))
+            {
+                while (!sr.EndOfStream)
+                {
+
+                    // Store last value as the key
+                    String[] line = sr.ReadLine().Split(',');
+                    keyList.Enqueue(line.Last().Trim('"'));
+                    rows++;
+
+                }
+
+            }
+
+            return rows;
+        }
+
+        private void FileButton_Click(object sender, EventArgs e)
+        {
+            if (openSampleFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.sampleFileName = openSampleFileDialog.FileName;
+
+                FileSelectedLabel.Text = Path.GetExtension(sampleFileName) + " file loaded";
+            }
+        }
+
+        private void loadKeyListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openKeyListFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                keyListFileName = openKeyListFileDialog.FileName;
+
+                // Load CSV into memory
+               int keys = LoadKeyList(keyListFileName);
+               DisplayText.Text = keys + " keys loaded";
+               Log("Loaded list \"" + Path.GetFileName(keyListFileName) + "\" with " + keys + " keys");
+
+            }
+        }
+    }
+}
